32c695c86662b6c756d6c3224c68323b2444d6ea
OlegPotapenko <PotapenkoOleg@gmail.com>
2025-12-08 19:54:18 -0500

    Version number increase 0.6.0

--- src/version.rs
+++ src/version.rs
@@ -1,9 +1,9 @@
 pub const PRODUCT_NAME: &str = "TACO";
 pub const VERSION_MAJOR: i32 = 0;
-pub const VERSION_MINOR: i32 = 5;
+pub const VERSION_MINOR: i32 = 6;
 pub const VERSION_PATCH: i32 = 0;
 pub const VERSION_ALIAS: &str = "Boston";
 pub const COPYRIGHT: &str = "Oleg Potapenko";
-pub const COPYRIGHT_YEARS: &str = "2024, 2025";
+pub const COPYRIGHT_YEARS: &str = "2024, 2025, 2026";
 pub const LICENSE: &str = "MIT";
 pub const LINK: &str = "https://github.com/PotapenkoOleg/taco";
